c2fc7baeebe6251b17d7f923e938f86ea5e8cbdd
tranquangduy1810 <tranquangduy1810@gmail.com>
2023-11-12 21:25:25 +0700

    commit update

--- Lab_3/training.py
+++ Lab_3/training.py
@@ -8,33 +8,45 @@ import torch.nn as nn
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
+# ./Deep-Learning/Lab_3/
+#Config
+#---------------------#
+train_image_path = './Deep-Learning/Lab_3/dataset/train-images-idx3-ubyte.gz'
+train_label_path = './Deep-Learning/Lab_3/dataset/train-labels-idx1-ubyte.gz'
+test_image_path = './Deep-Learning/Lab_3/dataset/t10k-images-idx3-ubyte.gz'
+test_label_path = './Deep-Learning/Lab_3/dataset/t10k-labels-idx1-ubyte.gz'
+n_epochs = 20
+batch_size_train = 128
+batch_size_test = 128
+learning_rate = 0.01
+#---------------------#
 
-training = MNISTDataset('dataset/train-images-idx3-ubyte.gz', 'dataset/train-labels-idx1-ubyte.gz')
-train_loader = DataLoader(training, batch_size=32, shuffle=True)
 
-test = MNISTDataset('dataset/t10k-images-idx3-ubyte.gz', 'dataset/t10k-labels-idx1-ubyte.gz')
-test_loader = DataLoader(test, batch_size=1, shuffle=True)
+training = MNISTDataset(train_image_path, train_label_path)
+test = MNISTDataset(test_image_path, test_label_path)
+
+train_loader = DataLoader(training, batch_size = batch_size_train, shuffle=True)
+test_loader = DataLoader(test, batch_size = batch_size_test, shuffle=True)
 
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(device)
 
 model = GoogLeNet().to(device)
 
 optimizer = torch.optim.SGD(
     model.parameters(),
-    lr=0.01
+    lr = learning_rate
 )
 
 loss_fn = nn.CrossEntropyLoss()
 
 # Train the model
-num_epochs = 20
-# total_step = len(train_loader)
 
 list_loss = []
 list_accuracy = []
 
-for epoch in range(num_epochs):
+for epoch in range(n_epochs):
     epoch_loss = 0.0
     epoch_acc = 0.0
 
@@ -56,11 +68,11 @@ for epoch in range(num_epochs):
         epoch_loss += loss.item()
         epoch_acc += acc
         
-        pbar.set_description(f'Epoch [{epoch+1}/{num_epochs}], loss: {epoch_loss / (i+1):.4f}, accuracy: {epoch_acc / (i+1):.4f}')
+        pbar.set_description(f'Epoch [{epoch+1}/{n_epochs}], loss: {epoch_loss / (i+1):.4f}, accuracy: {epoch_acc / (i+1):.4f}')
         
     epoch_loss /= len(train_loader)
     epoch_acc /= len(train_loader)
     list_loss.append(epoch_loss)
     list_accuracy.append(epoch_acc)
     
-    print('Epoch [{}/{}], loss: {:.4f}, accuracy: {:.4f}'.format(epoch+1, num_epochs, epoch_loss, epoch_acc))
+    print('Epoch [{}/{}], loss: {:.4f}, accuracy: {:.4f}'.format(epoch+1, n_epochs, epoch_loss, epoch_acc))
