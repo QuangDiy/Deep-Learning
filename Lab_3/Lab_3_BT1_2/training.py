e7545a6bed6d2d143f6c91b87821c02c895e865a
tranquangduy1810 <tranquangduy1810@gmail.com>
2023-11-13 20:41:26 +0700

    comit :)))

--- Lab_3/Lab_3_BT1_2/training.py
+++ Lab_3/Lab_3_BT1_2/training.py
@@ -27,6 +27,7 @@ batch_size_test = 128
 learning_rate = 0.01
 patience = 3
 momentum = 0.9
+shuffle = "True"
 #---------------------#
 
 training = MNISTDataset(train_image_path, train_label_path)
@@ -35,9 +36,9 @@ test = MNISTDataset(test_image_path, test_label_path)
 dev_size = int(len(training) * 0.2)
 train_indices, dev_indices = train_test_split(range(len(training)), test_size=dev_size, random_state = 42)
 
-train_loader = DataLoader(Subset(training, train_indices), batch_size=batch_size_train, shuffle = False)
-test_loader = DataLoader(test, batch_size = batch_size_test, shuffle = False)
-dev_loader = DataLoader(Subset(training, dev_indices), batch_size=batch_size_train, shuffle = False)
+train_loader = DataLoader(Subset(training, train_indices), batch_size=batch_size_train, shuffle = shuffle)
+test_loader = DataLoader(test, batch_size = batch_size_test, shuffle = shuffle)
+dev_loader = DataLoader(Subset(training, dev_indices), batch_size=batch_size_train, shuffle = shuffle)
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
