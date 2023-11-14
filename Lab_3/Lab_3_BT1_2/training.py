960cb3fdd8fca27a59039c93f2991faaf3996623
tranquangduy1810 <tranquangduy1810@gmail.com>
2023-11-14 21:23:21 +0700

    commit

--- Lab_3/Lab_3_BT1_2/training.py
+++ Lab_3/Lab_3_BT1_2/training.py
@@ -10,12 +10,8 @@ from tqdm import tqdm
 from sklearn.model_selection import train_test_split
 from torch.utils.data import Subset
 from utils.early_stopping import EarlyStopping
+from torch.optim.lr_scheduler import StepLR
 
-#---------------------#
-# train_image_path = 'dataset/train-images-idx3-ubyte.gz'
-# train_label_path = 'dataset/train-labels-idx1-ubyte.gz'
-# test_image_path = 'dataset/t10k-images-idx3-ubyte.gz'
-# test_label_path = 'dataset/t10k-labels-idx1-ubyte.gz'
 #---------------------#
 train_image_path = './Deep-Learning/Lab_3/Lab_3_BT1_2/dataset/train-images-idx3-ubyte.gz'
 train_label_path = './Deep-Learning/Lab_3/Lab_3_BT1_2/dataset/train-labels-idx1-ubyte.gz'
@@ -27,7 +23,8 @@ batch_size_test = 128
 learning_rate = 0.01
 patience = 3
 momentum = 0.9
-weight_decay = 0.01
+weight_decay = 1e-4
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 #---------------------#
 
 training = MNISTDataset(train_image_path, train_label_path)
@@ -36,19 +33,16 @@ test = MNISTDataset(test_image_path, test_label_path)
 dev_size = int(len(training) * 0.2)
 train_indices, dev_indices = train_test_split(range(len(training)), test_size=dev_size, random_state = 42)
 
-train_loader = DataLoader(Subset(training, train_indices), batch_size=batch_size_train, shuffle = False)
+train_loader = DataLoader(Subset(training, train_indices), batch_size=batch_size_train, shuffle = True)
 test_loader = DataLoader(test, batch_size = batch_size_test, shuffle = False)
 dev_loader = DataLoader(Subset(training, dev_indices), batch_size=batch_size_train, shuffle = False)
-# Device configuration
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-model = LeNet().to(device)
+model = GoogLeNet().to(device)
 
 optimizer = torch.optim.SGD(model.parameters(), lr = learning_rate, momentum = momentum, weight_decay = weight_decay)
-
-loss_fn = nn.CrossEntropyLoss()
-
 early_stopping = EarlyStopping(patience = patience, verbose=True)
+scheduler = StepLR(optimizer, step_size = 5, gamma = 0.1)
+loss_fn = nn.CrossEntropyLoss()
 
 list_loss = []
 list_accuracy = []
@@ -64,21 +58,21 @@ for epoch in range(n_epochs):
     for i, (images, labels) in pbar:
         images = images.to(device)
         labels = labels.to(device)
-
         outputs = model(images)
         loss = loss_fn(outputs, labels)
         acc = (torch.max(outputs, dim=1)[1] == labels).sum().item() / labels.size(0)
 
-        # Backward and optimize
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-
-        epoch_loss += loss.item()
-        epoch_acc += acc
         
+        epoch_loss += loss.item()
+        epoch_acc += acc        
         pbar.set_description(f'Epoch [{epoch+1}/{n_epochs}], loss: {epoch_loss / (i+1):.4f}, accuracy: {epoch_acc / (i+1):.4f}')
         
+    # Step the learning rate scheduler
+    scheduler.step()
+
     epoch_loss /= len(train_loader)
     epoch_acc /= len(train_loader)
     list_loss.append(epoch_loss)
@@ -108,13 +102,13 @@ for epoch in range(n_epochs):
 
     early_stopping(dev_loss, model)
     if early_stopping.early_stop:
-        n_epochs = epoch+1
+        n_epochs = epoch + 1
         print("Early stopping")
         break
 #---------------------#
 y_pred, y_true = predict(model, test_loader, device)
 acc, f1, precision, recall = compute_score(y_pred, y_true)
-metrics = classification_labels(y_pred, y_true, num_classes = 10)
+metrics = classification_labels(y_pred, y_true, num_classes)
 # Test set
 print("Accuracy: {:.4f} | F1 Score: {:.4f} | Precision: {:.4f} | Recall: {:.4f}".format(acc, f1, precision, recall))
 # For each labels
@@ -125,5 +119,3 @@ for i in range(len(metrics['f1'])):
 # save image
 save_fig(y_pred, y_true)
 save_loss_acc_plots(n_epochs, list_loss, list_accuracy, list_dev_loss, list_dev_accuracy)
-
-
