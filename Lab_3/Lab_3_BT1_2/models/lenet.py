8f99a310ffe466495321cb56efe55067c6c51d46
tranquangduy1810 <tranquangduy1810@gmail.com>
2023-11-14 22:01:04 +0700

    commit

--- Lab_3/Lab_3_BT1_2/models/lenet.py
+++ Lab_3/Lab_3_BT1_2/models/lenet.py
@@ -8,7 +8,7 @@ class LeNet(nn.Module):
         self.pooling1 = nn.MaxPool2d(kernel_size=2, stride=2)
         self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5)
         self.pooling2 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.linear1 = nn.Linear(16 * 6 * 6, 120)
+        self.linear1 = nn.Linear(16 * 5 * 5, 120)
         self.linear2 = nn.Linear(120, 84)
         self.linear3 = nn.Linear(84, num_classes)        
         
